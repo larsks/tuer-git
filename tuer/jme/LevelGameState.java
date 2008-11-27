d4bb6ed1fa3403e4e05634970a6d1cd0476b35d6
gouessej <gouessej@48496033-b546-4ea5-bad7-dea05dfac3c5>
2008-11-27 20:24:01 +0000

    Adds a missing class

---
+++ tuer/jme/LevelGameState.java
@@ -0,0 +1,22 @@
+package jme;
+
+import com.jmex.game.state.BasicGameState;
+import com.jmex.game.state.GameState;
+import com.jmex.game.state.load.TransitionGameState;
+
+public final class LevelGameState extends BasicGameState {
+
+    public LevelGameState(String name){
+        super(name);       
+    }
+
+    public static GameState getInstance(int index,
+            TransitionGameState transitionGameState){
+        //TODO: update the transition game state
+        //TODO: return a true level game state
+        return(new LevelGameState(""));
+    }
+    
+    
+
+}
