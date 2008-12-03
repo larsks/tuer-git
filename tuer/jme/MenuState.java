37c8dbeee8d4cb88e4355e0783a6c8873726c1e4
gouessej <gouessej@48496033-b546-4ea5-bad7-dea05dfac3c5>
2008-12-03 07:11:11 +0000

    First attempt of modifying the texture coordinates and some code cleaning

--- tuer/jme/MenuState.java
+++ tuer/jme/MenuState.java
@@ -1,11 +1,10 @@
 package jme;
 
 import java.net.URL;
-//import java.util.logging.Logger;
+import java.util.logging.Logger;
 import com.jme.scene.TexCoords;
 import com.jme.scene.state.TextureState;
 import com.jme.image.Texture;
-import com.jme.input.InputHandler;
 import com.jme.input.MouseInput;
 import com.jme.math.Vector3f;
 import com.jme.renderer.Renderer;
@@ -19,62 +18,18 @@ import com.jmex.game.state.load.TransitionGameState;
 
 public final class MenuState extends BasicGameState {
 
+
+    private Box[] menuItemArray;
     
-    private Box newGameBoxItem;
-    
-    private Box optionsItem;
-    
-    private Box loadGameItem;
-    
-    private Box saveGameItem;
-    
-    private Box aboutItem;
-    
-    private Box quitGameItem;
+    private static final String[] itemNameArray=new String[]{"New Game","Options","Save Game","Load Game","About","Quit"};
     
     private ExtendedMenuHandler input;
     
-    //private static final Logger logger = Logger.getLogger(MenuState.class.getName());
+    private static final Logger logger = Logger.getLogger(MenuState.class.getName());
     
     
     public MenuState(String name,final TransitionGameState trans,final JMEGameServiceProvider serviceProvider){
         super(name);
-        final float itemHeight=2.0f;
-        final float verticalInterItemGap=0.5f;
-        final float titleHeight=3.0f;
-        final float verticalInterSectionGap=1.0f;
-        float ordinate=(((6*itemHeight)+(5*verticalInterItemGap))/2)-titleHeight-verticalInterSectionGap;
-        this.newGameBoxItem=new Box("New Game Menu Item",new Vector3f(-5,ordinate-itemHeight,-5),new Vector3f(5,ordinate,5));
-        ordinate-=itemHeight+verticalInterItemGap;
-        this.optionsItem=new Box("Options Menu Item",new Vector3f(-5,ordinate-itemHeight,-5),new Vector3f(5,ordinate,5));
-        ordinate-=itemHeight+verticalInterItemGap;
-        this.saveGameItem=new Box("Save Game Menu Item",new Vector3f(-5,ordinate-itemHeight,-5),new Vector3f(5,ordinate,5));
-        ordinate-=itemHeight+verticalInterItemGap;
-        this.loadGameItem=new Box("About Menu Item",new Vector3f(-5,ordinate-itemHeight,-5),new Vector3f(5,ordinate,5));
-        ordinate-=itemHeight+verticalInterItemGap;
-        this.aboutItem=new Box("About Menu Item",new Vector3f(-5,ordinate-itemHeight,-5),new Vector3f(5,ordinate,5));
-        ordinate-=itemHeight+verticalInterItemGap;
-        this.quitGameItem=new Box("Quit Game Menu Item",new Vector3f(-5,ordinate-itemHeight,-5),new Vector3f(5,ordinate,5));
-        this.newGameBoxItem.setLocalTranslation(0,0,-10);
-        this.optionsItem.setLocalTranslation(0,0,-10);
-        this.saveGameItem.setLocalTranslation(0,0,-10);
-        this.loadGameItem.setLocalTranslation(0,0,-10);
-        this.aboutItem.setLocalTranslation(0,0,-10);
-        this.quitGameItem.setLocalTranslation(0,0,-10);
-        //TODO: add texture coordinates
-        //this.newGameBoxItem.addTextureCoordinates(new TexCoords());
-        this.newGameBoxItem.setRenderQueueMode(Renderer.QUEUE_OPAQUE);
-        this.optionsItem.setRenderQueueMode(Renderer.QUEUE_OPAQUE);
-        this.saveGameItem.setRenderQueueMode(Renderer.QUEUE_OPAQUE);
-        this.loadGameItem.setRenderQueueMode(Renderer.QUEUE_OPAQUE);
-        this.aboutItem.setRenderQueueMode(Renderer.QUEUE_OPAQUE);        
-        this.quitGameItem.setRenderQueueMode(Renderer.QUEUE_OPAQUE);
-        this.rootNode.attachChild(this.newGameBoxItem);
-        this.rootNode.attachChild(this.optionsItem);
-        this.rootNode.attachChild(this.saveGameItem);
-        this.rootNode.attachChild(this.loadGameItem);
-        this.rootNode.attachChild(this.aboutItem);
-        this.rootNode.attachChild(this.quitGameItem);
         Renderer renderer=DisplaySystem.getDisplaySystem().getRenderer();
         //TODO: use another texture for the menu
         URL quitItemTextureURL=ResourceLocatorTool.locateResource(ResourceLocatorTool.TYPE_TEXTURE,"starting_screen_bis.png");
@@ -82,11 +37,37 @@ public final class MenuState extends BasicGameState {
         ts.setEnabled(true);
         ts.setTexture(TextureManager.loadTexture(quitItemTextureURL,
                 Texture.MinificationFilter.BilinearNoMipMaps,
-                Texture.MagnificationFilter.Bilinear));        
-        this.rootNode.setRenderState(ts);
-        this.rootNode.updateRenderState();
+                Texture.MagnificationFilter.Bilinear));       
+        final float itemHeight=2.0f;
+        final float verticalInterItemGap=0.5f;
+        final float titleHeight=3.0f;
+        final float verticalInterSectionGap=1.0f;
+        final float xmin=-5.0f;
+        final float zmin=-5.0f;
+        final float xmax=5.0f;
+        final float zmax=5.0f;
+        final int itemCount=itemNameArray.length;
+        final int faceCount=6;
+        menuItemArray=new Box[itemCount];
+        float ordinate=(((itemCount*itemHeight)+((itemCount-1)*verticalInterItemGap))/2)-titleHeight-verticalInterSectionGap;
+        for(int itemIndex=0;itemIndex<itemCount;itemIndex++)
+            {menuItemArray[itemIndex]=new Box(itemNameArray[itemIndex]+" Menu Item",new Vector3f(xmin,ordinate-itemHeight,zmin),new Vector3f(xmax,ordinate,zmax));
+             ordinate-=itemHeight+verticalInterItemGap;
+             menuItemArray[itemIndex].setLocalTranslation(0,0,-10);
+             menuItemArray[itemIndex].setRenderQueueMode(Renderer.QUEUE_OPAQUE);
+             menuItemArray[itemIndex].setRenderState(ts);
+             for(int faceIndex=0;faceIndex<faceCount;faceIndex++)
+                 {menuItemArray[itemIndex].addTextureCoordinates(TexCoords.makeNew(new float[]{faceIndex/(float)faceCount,itemIndex/(float)itemCount}));
+                  menuItemArray[itemIndex].addTextureCoordinates(TexCoords.makeNew(new float[]{(faceIndex+1)/(float)faceCount,itemIndex/(float)itemCount}));
+                  menuItemArray[itemIndex].addTextureCoordinates(TexCoords.makeNew(new float[]{(faceIndex+1)/(float)faceCount,(itemIndex+1)/(float)itemCount}));
+                  menuItemArray[itemIndex].addTextureCoordinates(TexCoords.makeNew(new float[]{faceIndex/(float)faceCount,(itemIndex+1)/(float)itemCount})); 
+                  logger.info("TEXCOORDS: "+faceIndex/(float)faceCount+" "+itemIndex/(float)itemCount);
+                 }
+             rootNode.attachChild(menuItemArray[itemIndex]);
+            }   
+        rootNode.updateRenderState();
         //setup the input handler
-        this.input=new ExtendedMenuHandler(serviceProvider,this);
+        input=new ExtendedMenuHandler(serviceProvider,this);
     }
     
     /**
@@ -108,13 +89,13 @@ public final class MenuState extends BasicGameState {
     public final void render(final float tpf) {
         super.render(tpf);
         //TODO: use this index to set a different color for the selected menu
-        this.input.getIndex();
+        input.getIndex();
         //TODO: draw the menu
     }
     
     @Override
     public final void update(final float tpf) {
         super.update(tpf);
-        this.input.update(tpf);
+        input.update(tpf);
     }
 }
