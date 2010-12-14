4c2caa3445aafb9b56468e364df518f80bbdd86a
gouessej <gouessej@48496033-b546-4ea5-bad7-dea05dfac3c5>
2010-12-14 22:44:10 +0000

    Huge cleanup of the whole API

--- pre_beta/engine/data/AmmunitionUserData.java
+++ pre_beta/engine/data/AmmunitionUserData.java
@@ -1,6 +1,18 @@
+/*This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License
+  as published by the Free Software Foundation, version 2
+  of the License.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place - Suite 330, Boston,
+  MA 02111-1307, USA.
+*/
 package engine.data;
 
-import engine.statemachine.GameState;
 import engine.weaponry.Ammunition;
 
 public final class AmmunitionUserData extends CollectibleUserData{
@@ -10,8 +22,8 @@ public final class AmmunitionUserData extends CollectibleUserData{
 	
 	private final int ammunitionCount;
 	
-	public AmmunitionUserData(final Ammunition ammunition,final int ammunitionCount){
-		super(GameState.pickupAmmoSourcename);
+	public AmmunitionUserData(final String sourcename,final Ammunition ammunition,final int ammunitionCount){
+		super(sourcename);
 		this.ammunition=ammunition;
 		this.ammunitionCount=ammunitionCount;
 	}
