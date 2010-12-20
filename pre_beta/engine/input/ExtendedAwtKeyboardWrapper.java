57218a0ccbddaea3de842675c839d3dbfd1a2ff2
gouessej <gouessej@48496033-b546-4ea5-bad7-dea05dfac3c5>
2010-12-20 23:32:02 +0000

    Does not rely on the locale to detect the keyboard layout

--- pre_beta/engine/input/ExtendedAwtKeyboardWrapper.java
+++ pre_beta/engine/input/ExtendedAwtKeyboardWrapper.java
@@ -1,99 +1,126 @@
 package engine.input;
 
 import com.ardor3d.input.Key;
+import com.ardor3d.input.awt.AwtKey;
 import com.ardor3d.input.awt.AwtKeyboardWrapper;
 import java.awt.Component;
 import java.awt.event.KeyEvent;
-import java.util.Locale;
 
 public class ExtendedAwtKeyboardWrapper extends AwtKeyboardWrapper{
 	
-	public ExtendedAwtKeyboardWrapper(final Component component) {
+	enum KeyboardLayout{
+		AZERTY,QWERTY,UNKNOWN;		
+	};
+	
+	enum AzertyKeymap{
+		ZERO(19,KeyEvent.VK_UNDEFINED,-1),
+		ONE(10,KeyEvent.VK_AMPERSAND,-1),
+		TWO(11,KeyEvent.VK_UNDEFINED,-1),
+		THREE(12,KeyEvent.VK_QUOTEDBL,-1),
+		FOUR(13,KeyEvent.VK_QUOTE,-1),
+		FIVE(14,KeyEvent.VK_LEFT_PARENTHESIS,-1),
+		//it differs between the French and the Belgian versions
+		//SIX(15,KeyEvent.VK_MINUS,KeyEvent.),
+		SEVEN(16,KeyEvent.VK_UNDEFINED,-1),
+		//it differs between the French and the Belgian versions
+		EIGHT(17,KeyEvent.VK_UNDERSCORE,KeyEvent.VK_EXCLAMATION_MARK),
+		NINE(18,KeyEvent.VK_UNDEFINED,-1);
+		
+		private final int awtRawCode;
+		/**key code of the French variant*/
+		private final int awtKeyCode;
+		/**key code of the Belgian variant*/
+		private final int awtSecondaryKeyCode;
+		
+		private AzertyKeymap(final int awtRawCode,final int awtKeyCode,final int awtSecondaryKeyCode){
+			this.awtRawCode=awtRawCode;
+			this.awtKeyCode=awtKeyCode;
+			this.awtSecondaryKeyCode=awtSecondaryKeyCode;
+		}
+	};
+	
+	enum QwertyKeymap{
+		ZERO(19,KeyEvent.VK_0),
+		ONE(10,KeyEvent.VK_1),
+		TWO(11,KeyEvent.VK_2),
+		THREE(12,KeyEvent.VK_3),
+		FOUR(13,KeyEvent.VK_4),
+		FIVE(14,KeyEvent.VK_5),
+		SIX(15,KeyEvent.VK_6),
+		SEVEN(16,KeyEvent.VK_7),
+		EIGHT(17,KeyEvent.VK_8),
+		NINE(18,KeyEvent.VK_9);
+		
+		private final int awtRawCode;
+		
+		private final int awtKeyCode;
+		
+		private QwertyKeymap(final int awtRawCode,final int awtKeyCode){
+			this.awtRawCode=awtRawCode;
+			this.awtKeyCode=awtKeyCode;
+		}
+	};
+	
+	private KeyboardLayout keyboardLayout;
+	
+	public ExtendedAwtKeyboardWrapper(final Component component){
 		super(component);
+		keyboardLayout=null;
+	}
+	
+	private void detectKeyboardLayout(final KeyEvent e){
+		if(this.keyboardLayout==null&&e.getKeyLocation()!=KeyEvent.KEY_LOCATION_NUMPAD)
+		    {final int rawcode=getKeyRawCode(e);
+		     final int keycode=e.getKeyCode();
+			 for(QwertyKeymap keymapValue:QwertyKeymap.values())
+	        	 if(keymapValue.awtRawCode==rawcode&&keymapValue.awtKeyCode==keycode)
+	        	     {this.keyboardLayout=KeyboardLayout.QWERTY;
+	        		  break;
+	        	     }
+			 if(this.keyboardLayout==null)
+			     {for(AzertyKeymap keymapValue:AzertyKeymap.values())
+	        	      if(keymapValue.awtRawCode==rawcode&&
+	        	        (keymapValue.awtKeyCode==keycode||
+	        	         keymapValue.awtSecondaryKeyCode==keycode))
+	        	          {this.keyboardLayout=KeyboardLayout.AZERTY;
+	        		       break;
+	        	          }				  
+			     }
+		    }
 	}
 
+	private final int getKeyRawCode(final KeyEvent e){
+		final String paramString=e.paramString();
+		final String[] splittedParamString=paramString.split(",");
+		int rawcode=-1;
+		for(String keyValuePair:splittedParamString)
+		    if(keyValuePair.startsWith("rawCode")||keyValuePair.startsWith("rawcode"))
+		        {rawcode=Integer.parseInt(keyValuePair.substring(8));
+			     break;
+		        }
+		return(rawcode);
+	}
+	
 	@Override
 	public final synchronized Key fromKeyEventToKey(final KeyEvent e){
-		final Locale defaultLocale = Locale.getDefault();
-        final String country = defaultLocale.getCountry();
-        Key key = null;
-        if(e.getKeyLocation() != KeyEvent.KEY_LOCATION_NUMPAD && 
-        		defaultLocale.getLanguage().equals("fr")
-                && (country.equals("FR") || country.equals("BE")))
-            switch(e.getKeyChar())
-                {case '1':
-                     if(e.getKeyCode() == KeyEvent.VK_AMPERSAND)
-                    	 key=Key.ONE;                  
-                     break;
-                 case '&':
-                	 key=Key.ONE;
-                	 break;
-                 case '2':
-                     if(e.getKeyCode() == KeyEvent.VK_UNDEFINED)
-                         key=Key.TWO;                   
-                     break;
-                 case 'é':
-                	 key=Key.TWO;
-                	 break;
-                 case '3':
-                     if(e.getKeyCode() == KeyEvent.VK_QUOTEDBL)
-                    	 key=Key.THREE;                   
-                     break;
-                 case '\"':
-                	 key=Key.THREE;
-                	 break;
-                 case '4':
-                     if(e.getKeyCode() == KeyEvent.VK_QUOTE)
-                    	 key=Key.FOUR;                   
-                     break;                   
-                 case '\'':
-                	 key=Key.FOUR;
-                	 break;
-                 case '5':
-                     if(e.getKeyCode() == KeyEvent.VK_LEFT_PARENTHESIS)
-                    	 key=Key.FIVE;
-                     break;
-                 case '(':
-                	 key=Key.FIVE;
-                	 break;
-                 case '6':
-                     if(e.getKeyCode() == KeyEvent.VK_MINUS)
-                    	 key=Key.SIX;                   
-                     break;                   
-                 case '-':
-                	 key=Key.SIX;
-                	 break;
-                 case '7':
-                     if(e.getKeyCode() == KeyEvent.VK_UNDEFINED)
-                    	 key=Key.SEVEN;                  
-                     break;
-                 case 'è':
-                	 key=Key.SEVEN;
-                	 break;
-                 case '8':
-                     if(e.getKeyCode() == KeyEvent.VK_UNDERSCORE)
-                    	 key=Key.EIGHT;
-                     break;                    
-                 case '_':
-                	 key=Key.EIGHT;
-                	 break;
-                 case '9':
-                     if(e.getKeyCode() == KeyEvent.VK_UNDEFINED)
-                    	 key=Key.NINE;
-                     break;                    
-                 case 'ç':
-                	 key=Key.NINE;
-                	 break;
-                 case '0':
-                     if(e.getKeyCode() == KeyEvent.VK_UNDEFINED)
-                    	 key=Key.ZERO;
-                     break;                    
-                 case 'à':
-                	 key=Key.ZERO;
-                	 break;
-                }          
-        if(key==null)
-            key=super.fromKeyEventToKey(e);
+		if(keyboardLayout==null)
+			detectKeyboardLayout(e);
+		//if(keyboardLayout!=null)
+		//	  System.out.println(keyboardLayout);
+		Key key=null;
+		if(keyboardLayout==null||keyboardLayout!=KeyboardLayout.QWERTY)
+		    {final int rawcode=getKeyRawCode(e);
+		     //overrides top-left key codes for numbers
+		     if(10<=rawcode&&rawcode<=19)
+		         {for(QwertyKeymap keymapValue:QwertyKeymap.values())
+		        	  if(keymapValue.awtRawCode==rawcode)
+		                  {key=AwtKey.findByCode(keymapValue.awtKeyCode);
+		        	       break;
+		                  }			      
+		         }			 
+		    }
+		if(key==null)
+			key=super.fromKeyEventToKey(e);
 		return(key);
 	}
 }
