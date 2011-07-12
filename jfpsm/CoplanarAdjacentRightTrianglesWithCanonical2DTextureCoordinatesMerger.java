bab3b2dc058d50136db86ea1070e20e7e660100d
gouessej <gouessej@48496033-b546-4ea5-bad7-dea05dfac3c5>
2011-07-12 23:14:55 +0000

    Adds a mesh optimizer which merges coplanar adjacent right triangles whose 2D texture coordinates are canonical (work in progress)

--- jfpsm/CoplanarAdjacentRightTrianglesWithCanonical2DTextureCoordinatesMerger.java
+++ jfpsm/CoplanarAdjacentRightTrianglesWithCanonical2DTextureCoordinatesMerger.java
@@ -13,9 +13,19 @@
 */
 package jfpsm;
 
+import java.util.ArrayList;
 import java.util.EnumSet;
+import java.util.HashMap;
 
+import com.ardor3d.math.Plane;
+import com.ardor3d.math.Triangle;
+import com.ardor3d.math.Vector2;
+import com.ardor3d.math.Vector3;
+import com.ardor3d.math.type.ReadOnlyVector3;
+import com.ardor3d.renderer.IndexMode;
 import com.ardor3d.scenegraph.Mesh;
+import com.ardor3d.scenegraph.MeshData;
+import com.ardor3d.util.geom.BufferUtils;
 import com.ardor3d.util.geom.GeometryTool;
 import com.ardor3d.util.geom.VertMap;
 import com.ardor3d.util.geom.GeometryTool.MatchCondition;
@@ -28,25 +38,165 @@ import com.ardor3d.util.geom.GeometryTool.MatchCondition;
  * @author Julien Gouesse
  *
  */
-public class CoplanarAdjacentRightTrianglesWithCanonical2DTextureCoordinatesMerger {
+public class CoplanarAdjacentRightTrianglesWithCanonical2DTextureCoordinatesMerger{
 
+	private static final class RightTriangleInfo{
+		
+		private final int primitiveIndex;
+		
+		private final int sectionIndex;
+		
+		private final int sideIndexOfHypotenuse;
+		
+		
+		private RightTriangleInfo(int primitiveIndex,int sectionIndex,int sideIndexOfHypotenuse){
+			this.primitiveIndex=primitiveIndex;
+			this.sectionIndex=sectionIndex;
+			this.sideIndexOfHypotenuse=sideIndexOfHypotenuse;
+		}
+	}
+	
 	/**
 	 * 
 	 * @param mesh using the same set of textures (only on texture per unit) for all vertices
 	 * @return
 	 */
 	public static VertMap minimizeVerts(final Mesh mesh) {
-		//FIXME split the mesh into several meshes using different texture states
-		EnumSet<MatchCondition> conditions = EnumSet.of(MatchCondition.UVs, MatchCondition.Normal, MatchCondition.Color);
-		VertMap result = GeometryTool.minimizeVerts(mesh, conditions);
-	    //TODO use VertGroupData vertices groups to sort vertices by textures or pass a mesh using a single texture
-	    //TODO use all conditions with GeometryTool (except the group condition if the mesh uses a single texture)
-	    //TODO separate right triangles with canonical 2D texture coordinates from the others
-	    //TODO sort the triangles of the former set by planes (4D: normalized normal + distance to plane)
-	    //TODO subdivide the sets so that each new set contains at most 2 triangles with the same hypotenuse (used to make quads)
-	    //TODO merge all sets containing adjacent quads
-	    //TODO merge as much triangles of each set as possible if it contains more than 4 triangles (2 quads) by maximizing their areas 
-	    //     (update their texture coordinates (use coordinates greater than 1) in order to use texture repeat)
+		//uses all conditions with GeometryTool
+		final EnumSet<MatchCondition> conditions=EnumSet.of(MatchCondition.UVs, MatchCondition.Normal, MatchCondition.Color);
+		//reduces the geometry to avoid duplication of vertices
+		final VertMap result=GeometryTool.minimizeVerts(mesh, conditions);
+		
+		final MeshData meshData=mesh.getMeshData();
+		//if there is a texture buffer for the first texture unit
+		if(meshData.getTextureBuffer(0)!=null)
+		    {//first step: separate right triangles with canonical 2D texture coordinates from the others
+			 final ArrayList<RightTriangleInfo> rightTrianglesWithCanonical2DTextureCoordinatesInfos=new ArrayList<CoplanarAdjacentRightTrianglesWithCanonical2DTextureCoordinatesMerger.RightTriangleInfo>();
+			 //loops on all sections of the mesh data
+			 Vector3[] triangleVertices=new Vector3[3];
+			 int[] triangleIndices=new int[3];
+			 Vector2[] triangleTextureCoords=new Vector2[3];
+			 final double[] trianglesSidesDistancesSquared=new double[3];
+			 double u,v;
+			 int sideIndexOfHypotenuse;
+			 boolean hasCanonicalTextureCoords;
+			 for(int sectionIndex=0,sectionCount=meshData.getSectionCount();sectionIndex<sectionCount;sectionIndex++)
+			     //only takes care of sections containing triangles
+				 if(meshData.getIndexMode(sectionIndex)==IndexMode.Triangles)
+			         {//loops on all triangles of each section
+					  for(int trianglePrimitiveIndex=0,triangleCount=meshData.getPrimitiveCount(sectionIndex);trianglePrimitiveIndex<triangleCount;trianglePrimitiveIndex++)
+				          {//gets the 3 vertices of the triangle
+						   triangleVertices=meshData.getPrimitive(trianglePrimitiveIndex,sectionIndex,triangleVertices);
+				    	   //uses Pythagorean theorem to check whether the triangle is right
+						   //computes the squared distances of all sides
+						   for(int triangleSideIndex=0;triangleSideIndex<3;triangleSideIndex++)
+						       trianglesSidesDistancesSquared[triangleSideIndex]=triangleVertices[triangleSideIndex].distanceSquared(triangleVertices[(triangleSideIndex+1)%3]);
+						   //uses these squared distances to find the hypotenuse if any
+						   sideIndexOfHypotenuse=-1;						   
+						   for(int triangleSideIndex=0;triangleSideIndex<3&&sideIndexOfHypotenuse==-1;triangleSideIndex++)
+                               if(trianglesSidesDistancesSquared[triangleSideIndex]==trianglesSidesDistancesSquared[(triangleSideIndex+1)%3]+trianglesSidesDistancesSquared[(triangleSideIndex+2)%3])
+							       sideIndexOfHypotenuse=triangleSideIndex;						   
+						   //if this triangle is right
+						   if(sideIndexOfHypotenuse!=-1)
+						       {//checks whether its texture coordinates are canonical
+							    hasCanonicalTextureCoords=true;
+							    for(int textureIndex=0;meshData.getTextureBuffer(textureIndex)!=null&&hasCanonicalTextureCoords;textureIndex++){
+							    	triangleTextureCoords=getPrimitiveTextureCoords(meshData,trianglePrimitiveIndex,sectionIndex,textureIndex,triangleTextureCoords);		    
+								    for(int triangleTextureCoordsIndex=0;triangleTextureCoordsIndex<3&&hasCanonicalTextureCoords;triangleTextureCoordsIndex++)
+								        {u=triangleTextureCoords[triangleTextureCoordsIndex].getX();
+								         v=triangleTextureCoords[triangleTextureCoordsIndex].getY();
+								         if((u!=0&&u!=1)||(v!=0&&v!=1))
+								        	 hasCanonicalTextureCoords=false;
+								        }
+							    }	    
+							    if(hasCanonicalTextureCoords)
+							        {//stores the side index of its hypotenuse and several indices allowing to retrieve the required data further 
+							         RightTriangleInfo rightTriangleInfo=new RightTriangleInfo(trianglePrimitiveIndex,sectionIndex,sideIndexOfHypotenuse);
+							         rightTrianglesWithCanonical2DTextureCoordinatesInfos.add(rightTriangleInfo);
+							        }
+						       }
+				          }
+			         }
+			 //second step: sorts the triangles of the former set by planes (4D: normal + distance to plane)
+			 HashMap<Plane,ArrayList<RightTriangleInfo>> mapOfTrianglesByPlanes=new HashMap<Plane, ArrayList<RightTriangleInfo>>();
+			 Triangle tmpTriangle=new Triangle();
+			 ReadOnlyVector3 triangleNormal;
+			 double distanceToPlane;
+			 for(RightTriangleInfo info:rightTrianglesWithCanonical2DTextureCoordinatesInfos)
+			     {//gets the 3 vertices of the triangle
+				  triangleVertices=meshData.getPrimitive(info.primitiveIndex,info.sectionIndex,triangleVertices);		  
+				  //sets the vertices of the temporary triangle
+				  for(int vertexInternalIndex=0;vertexInternalIndex<3;vertexInternalIndex++)
+				      tmpTriangle.set(vertexInternalIndex,triangleVertices[vertexInternalIndex]);
+				  //computes its normal
+				  triangleNormal=tmpTriangle.getNormal();
+				  //computes its distance to plane d=dot(normal,vertex)
+				  distanceToPlane=triangleNormal.dot(tmpTriangle.getCenter());
+				  //creates the plane
+				  final Plane plane=new Plane(new Vector3(triangleNormal),distanceToPlane);
+				  //puts it into a map whose key is a given plane
+				  ArrayList<RightTriangleInfo> infoList=mapOfTrianglesByPlanes.get(plane);
+				  if(infoList==null)
+				      {infoList=new ArrayList<RightTriangleInfo>();
+				       mapOfTrianglesByPlanes.put(plane,infoList);
+				      }
+				  infoList.add(info);		  
+			     }			 
+			 //TODO: third step: retain only triangles by pairs, each pair of triangles shares the same hypotenuse 
+		     //TODO for each plane of the map, for each RightTriangleInfo instance, find another instance whose hypotenuse is the same (the vertices might be in a different order), otherwise remove this instance from the list of this plane
+			 //TODO: fourth step: merge all sets containing adjacent quads
+		     //TODO create a list of lists of adjacent pairs of triangles inside the same plane
+			 //TODO: fifth step: merge as much triangles of each set as possible if it contains more than 4 triangles (2 quads) by maximizing their areas 
+		     //     (update their texture coordinates (use coordinates greater than 1) in order to use texture repeat)			 
+		    }
 		return result;
 	}
+	
+	/**
+     * Gets the texture coordinates of the primitive.
+     * 
+     * @param primitiveIndex
+     *            the primitive index
+     * @param section
+     *            the section
+     * @param textureIndex
+     *            the texture index
+     * @param store
+     *            the store
+     * 
+     * @return the texture coordinates of the primitive
+     */
+    public static Vector2[] getPrimitiveTextureCoords(final MeshData meshData, final int primitiveIndex, final int section, final int textureIndex, final Vector2[] store) {
+    	if (meshData.getTextureBuffer(textureIndex) == null) {
+    	    return null;
+    	}
+    	final int count = meshData.getPrimitiveCount(section);
+        if (primitiveIndex >= count || primitiveIndex < 0) {
+            throw new IndexOutOfBoundsException("Invalid primitiveIndex '" + primitiveIndex + "'.  Count is " + count);
+        }
+
+        final IndexMode mode = meshData.getIndexMode(section);
+        final int rSize = mode.getVertexCount();
+        Vector2[] result = store;
+        if (result == null || result.length < rSize) {
+            result = new Vector2[rSize];
+        }
+
+        for (int i = 0; i < rSize; i++) {
+        	if (result[i] == null) {
+        		result[i] = new Vector2();
+        	}
+        	if (meshData.getIndexBuffer() != null) {
+        		// indexed geometry
+        		BufferUtils.populateFromBuffer(result[i], meshData.getTextureBuffer(textureIndex),
+        				meshData.getIndices().get(meshData.getVertexIndex(primitiveIndex, i, section)));
+        	} else {
+        		// non-indexed geometry
+        		BufferUtils
+        		.populateFromBuffer(result[i], meshData.getTextureBuffer(textureIndex), meshData.getVertexIndex(primitiveIndex, i, section));
+        	}
+        }
+
+        return result;
+    }
 }
