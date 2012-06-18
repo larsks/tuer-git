e464771dd73109feb60bb21ad51fffc866ab9d4d
gouessej <gouessej@48496033-b546-4ea5-bad7-dea05dfac3c5>
2012-06-18 19:48:15 +0000

    Allows to display several images during the loading of the level

--- pre_beta/engine/statemachine/LoadingDisplayState.java
+++ pre_beta/engine/statemachine/LoadingDisplayState.java
@@ -24,6 +24,7 @@ import com.ardor3d.input.logical.TriggerAction;
 import com.ardor3d.math.Vector3;
 import com.ardor3d.renderer.Camera;
 import com.ardor3d.renderer.state.TextureState;
+import com.ardor3d.renderer.state.RenderState.StateType;
 import com.ardor3d.scenegraph.Spatial;
 import com.ardor3d.scenegraph.controller.SpatialController;
 import com.ardor3d.scenegraph.shape.Box;
@@ -43,6 +44,10 @@ public final class LoadingDisplayState extends ScenegraphState{
     private final TaskManager taskManager;
     
     private final Box box;
+    
+    private final Texture[] textures;
+    
+    private final String[] texturesPaths;
 
     
     public LoadingDisplayState(final NativeCanvas canvas,final PhysicalLayer physicalLayer,final TriggerAction exitAction,final TriggerAction toGameAction,final SoundManager soundManager,final TaskManager taskManager){
@@ -52,6 +57,8 @@ public final class LoadingDisplayState extends ScenegraphState{
         final Camera cam=canvas.getCanvasRenderer().getCamera();
         taskNode.setTranslation(0,-cam.getHeight()/2.5,0);
         getRoot().attachChild(taskNode);
+        texturesPaths=new String[]{"communism.png","venimus_vidimus_vicimus.png"};
+        textures=new Texture[texturesPaths.length];
         // execute tasks
         taskNode.addController(new SpatialController<Spatial>(){
         	
@@ -59,9 +66,16 @@ public final class LoadingDisplayState extends ScenegraphState{
         	
         	boolean firstTaskOk=false;
         	
+        	int textureIndex=0;
+        	
+        	int taskCount=0;
+        	
             @Override
             public final void update(final double time,final Spatial caller){
-            	//perform the long task only at the second update to display the task node correctly
+            	//updates the texture
+       	        final TextureState textureState=(TextureState)box.getLocalRenderState(StateType.Texture);
+       	        textureState.setTexture(textures[textureIndex]);
+            	//performs the long task only at the second update to display the task node correctly
             	if(!oneSkipDone)
             		oneSkipDone=true;
             	else
@@ -71,11 +85,14 @@ public final class LoadingDisplayState extends ScenegraphState{
             		          {firstTaskOk=true;
             		           //the first task is used to enqueue all loading tasks
             		           taskNode.reset();
+            		           taskCount=taskManager.getTaskCount();
             		          }
+            		      if(taskManager.getTaskCount()>0)
+            		    	  textureIndex=(int)Math.floor(((taskCount-taskManager.getTaskCount())/(double)taskCount)*textures.length);
             		     }
             	     else
                          toGameAction.perform(null,null,-1);
-            	    }
+            	    }            	
             }
         });
         box=new Box("Level Loading Display Box",Vector3.ZERO,5,5,5);
@@ -98,8 +115,10 @@ public final class LoadingDisplayState extends ScenegraphState{
     	// puts a texture onto the box
         TextureState ts=new TextureState();
         ts.setEnabled(true);
-        ts.setTexture(TextureManager.load("communism.png",Texture.MinificationFilter.Trilinear,true));
+        //ts.setTexture(TextureManager.load("communism.png",Texture.MinificationFilter.Trilinear,true));
         box.setRenderState(ts);
+        for(int textureIndex=0;textureIndex<texturesPaths.length;textureIndex++)
+        	textures[textureIndex]=TextureManager.load(texturesPaths[textureIndex],Texture.MinificationFilter.Trilinear,true);
     }
     
     public final void setLevelInitializationTask(final Runnable levelInitializationTask){
