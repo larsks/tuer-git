2499bb2f40cb2c6b3f1577df6cbe7d177f480fe7
gouessej <gouessej@48496033-b546-4ea5-bad7-dea05dfac3c5>
2012-08-14 20:56:55 +0000

    Implements the removal of vertices from a directed rooted tree

---
+++ jfpsm/graph/DirectedConnectedComponentVisitorWithHistory.java
@@ -0,0 +1,52 @@
+/*This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License
+  as published by the Free Software Foundation, version 2
+  of the License.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place - Suite 330, Boston,
+  MA 02111-1307, USA.
+*/
+package jfpsm.graph;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Object used to traverse the connected component of a directed graph that 
+ * stores all visited vertices
+ * 
+ * @author Julien Gouesse
+ *
+ */
+public class DirectedConnectedComponentVisitorWithHistory<V, E> extends
+		DirectedConnectedComponentVisitor<V, E> {
+
+	private final List<V> visitedVertices;
+	
+	/**
+	 * Constructor
+	 */
+	public DirectedConnectedComponentVisitorWithHistory(){
+		super();
+		visitedVertices=new ArrayList<V>();
+	}
+
+	/* (non-Javadoc)
+	 * @see jfpsm.graph.Visitor#performOnCurrentlyVisitedVertex(java.lang.Object)
+	 */
+	@Override
+	protected boolean performOnCurrentlyVisitedVertex(V currentlyVisitedVertex){
+		visitedVertices.add(currentlyVisitedVertex);
+		return(true);
+	}
+
+	public List<V> getVisitedVertices(){
+		return(Collections.unmodifiableList(visitedVertices));
+	}
+}
