5ff0fe981cb43d707e29d948d8d2f61973cc460c
gouessej <gouessej@48496033-b546-4ea5-bad7-dea05dfac3c5>
2014-07-16 21:24:14 +0000

    Puts the code that manages the creation of a tab component in the entity viewer into a separate method and improves the appearance of the button used to close a tab (work in progress)

--- jfpsm/EntityViewer.java
+++ jfpsm/EntityViewer.java
@@ -17,20 +17,26 @@
  */
 package jfpsm;
 
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
-import java.awt.Insets;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.HashMap;
+import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
+import javax.swing.plaf.basic.BasicButtonUI;
 
 /**
- * Widget that displays the entities. For example, a floor has a container map, a light map, a contained map and path maps.
- * This widget allows editing too.
+ * Component that displays the viewers of the entities in a tabbed pane
+ * 
  * @author Julien Gouesse
  *
  */
@@ -57,6 +63,36 @@ public class EntityViewer extends JPanel{
         add(entityTabbedPane);
     }
     
+    protected JPanel createEntityViewTabComponent(final JFPSMUserObject entity,final Viewer entityView){
+    	entityTabbedPane.addTab(entity.getName(),entityView);
+    	final JPanel tabComponent=new JPanel(new FlowLayout(FlowLayout.LEFT,0,0));
+        entityToTabComponentMap.put(entity,tabComponent);
+        tabComponent.setOpaque(false);
+        //adds much space to the top of the component
+        tabComponent.setBorder(BorderFactory.createEmptyBorder(2,0,0,0));
+        final JLabel label=new JLabel(entity.getName());
+        //adds much space between the label and the button
+        label.setBorder(BorderFactory.createEmptyBorder(0,0,0,5));
+        tabComponent.add(label);
+        //adds a button to close the tab of the viewer
+        final CloseButton closeButton=new CloseButton();
+        closeButton.addActionListener(new ActionListener(){               
+            @Override
+            public final void actionPerformed(ActionEvent ae){
+      	      closeEntityView(entity);
+            }
+        });
+        tabComponent.add(closeButton);
+        entityTabbedPane.setTabComponentAt(entityTabbedPane.indexOfComponent(entityView),tabComponent);
+        return(tabComponent);
+    }
+    
+    /**
+     * Opens the view of an entity. Creates it if it does not exist yet and then selects it
+     * 
+     * @param entity entity to view
+     * @return
+     */
     public boolean openEntityView(final JFPSMToolUserObject entity){
     	final boolean success;
     	JPanel tabComponent=entityToTabComponentMap.get(entity);
@@ -64,22 +100,7 @@ public class EntityViewer extends JPanel{
     	if(tabComponent==null)
     	    {entityView=entity.createViewer(toolManager);
     	     if(entityView!=null)
-    	         {entityTabbedPane.addTab(entity.getName(),entityView);
-                  tabComponent=new JPanel(new FlowLayout(FlowLayout.LEFT,3,0));
-                  entityToTabComponentMap.put(entity,tabComponent);
-                  tabComponent.setOpaque(false);
-                  tabComponent.add(new JLabel(entity.getName()));
-                  final JButton closeButton=new JButton("x");
-                  closeButton.addActionListener(new ActionListener(){               
-                      @Override
-                      public final void actionPerformed(ActionEvent e){
-                	      closeEntityView(entity);
-                      }
-                  });
-                  //removes all margins
-                  closeButton.setMargin(new Insets(0,0,0,0));
-                  tabComponent.add(closeButton);
-                  entityTabbedPane.setTabComponentAt(entityTabbedPane.indexOfComponent(entityView),tabComponent);
+    	         {tabComponent=createEntityViewTabComponent(entity,entityView);
                   success=true;
     	        }
     	     else
@@ -88,19 +109,20 @@ public class EntityViewer extends JPanel{
     	    	 }
     	    }
     	else
-    		{//the view of this entity is already open, selects it
-    		 entityTabbedPane.setSelectedIndex(entityTabbedPane.indexOfTabComponent(tabComponent));
+    		{//the view of this entity is already open, there is nothing to create
     		 success=true;
     		}
+    	if(tabComponent!=null)
+    		entityTabbedPane.setSelectedIndex(entityTabbedPane.indexOfTabComponent(tabComponent));
         return(success);
     }
     
     /**
-     * Opens the view of an entity. Creates it if it does not exist yet, otherwise selects it
+     * Opens the view of an entity. Creates it if it does not exist yet and then selects it
      * 
      * @param entity entity to view
      * @param project project in which this entity is
-     * @return
+     * @return 
      */
     public boolean openEntityView(final JFPSMProjectUserObject entity,final Project project){
     	final boolean success;
@@ -109,22 +131,7 @@ public class EntityViewer extends JPanel{
     	if(tabComponent==null)
     	    {entityView=entity.createViewer(project,projectManager);
     	     if(entityView!=null)
-    	         {entityTabbedPane.addTab(entity.getName(),entityView);
-                  tabComponent=new JPanel(new FlowLayout(FlowLayout.LEFT,3,0));
-                  entityToTabComponentMap.put(entity,tabComponent);
-                  tabComponent.setOpaque(false);
-                  tabComponent.add(new JLabel(entity.getName()));
-                  final JButton closeButton=new JButton("x");
-                  closeButton.addActionListener(new ActionListener(){               
-                      @Override
-                      public final void actionPerformed(ActionEvent e){
-                	      closeEntityView(entity);
-                      }
-                  });
-                  //removes all margins
-                  closeButton.setMargin(new Insets(0,0,0,0));
-                  tabComponent.add(closeButton);
-                  entityTabbedPane.setTabComponentAt(entityTabbedPane.indexOfComponent(entityView),tabComponent);
+    	         {tabComponent=createEntityViewTabComponent(entity,entityView);
                   success=true;
     	        }
     	     else
@@ -133,10 +140,11 @@ public class EntityViewer extends JPanel{
     	    	 }
     	    }
     	else
-    		{//the view of this entity is already open, selects it
-    		 entityTabbedPane.setSelectedIndex(entityTabbedPane.indexOfTabComponent(tabComponent));
-    		 success=true;
-    		}
+		    {//the view of this entity is already open, there is nothing to create
+		     success=true;
+		    }
+	    if(tabComponent!=null)
+		    entityTabbedPane.setSelectedIndex(entityTabbedPane.indexOfTabComponent(tabComponent));
         return(success);
     }
     
@@ -156,5 +164,45 @@ public class EntityViewer extends JPanel{
     	     entityToTabComponentMap.remove(entity);
     	    }
     	return(success);
+    }
+    
+    private static final class CloseButton extends JButton{
+    	
+    	private static final long serialVersionUID = 1L;
+    	
+    	private CloseButton(){
+    		super();
+    		setPreferredSize(new Dimension(20,20));
+            //uses the most basic UI whatever the look and feel
+            setUI(new BasicButtonUI());
+            //drives it transparent
+            setContentAreaFilled(false);
+            //only the rest of the tab should be focusable
+            setFocusable(false);
+            //sets a border that is never painted
+            setBorder(BorderFactory.createEmptyBorder(0,0,0,0));
+            setBorderPainted(false);
+            //enables the roll over so that we can use it while drawing
+            setRolloverEnabled(true);
+    	}
+		
+		@Override
+		protected void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            final Graphics2D g2=(Graphics2D)g.create();
+            if(getModel().isPressed())
+                g2.setStroke(new BasicStroke(2.0f,BasicStroke.CAP_BUTT,BasicStroke.JOIN_MITER));
+            else
+            	g2.setStroke(new BasicStroke(1.0f,BasicStroke.CAP_BUTT,BasicStroke.JOIN_MITER));
+            if (getModel().isRollover())
+                g2.setColor(Color.RED);
+            else
+            	g2.setColor(Color.BLACK);
+            //FIXME there is still a problem with the drawing of the crossed lines
+            final int delta=3;
+            g2.drawLine(delta,delta,getWidth()-delta-1,getHeight()-delta-1);
+            g2.drawLine(getWidth()-delta-1,delta,delta,getHeight()-delta-1);
+            g2.dispose();
+        }
     }
 }
