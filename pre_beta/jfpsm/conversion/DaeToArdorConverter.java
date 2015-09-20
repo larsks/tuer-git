2af60c2cc3f8cf5b10c9514ccd5caf315e89b4f1
gouessej <gouessej@48496033-b546-4ea5-bad7-dea05dfac3c5>
2015-09-20 15:29:42 +0000

    Moves the conversion classes from the game into the editor

--- pre_beta/jfpsm/conversion/DaeToArdorConverter.java
+++ pre_beta/jfpsm/conversion/DaeToArdorConverter.java
@@ -15,63 +15,53 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
-package engine.conversion;
+package jfpsm.conversion;
 
 import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
-import com.ardor3d.extension.model.md2.Md2Importer;
-import com.ardor3d.image.Texture;
+
+import jfpsm.GeometryHelper;
+
+import com.ardor3d.extension.model.collada.jdom.ColladaImporter;
 import com.ardor3d.image.util.jogl.JoglImageLoader;
-import com.ardor3d.renderer.state.TextureState;
-import com.ardor3d.scenegraph.Spatial;
-import com.ardor3d.util.TextureManager;
+import com.ardor3d.scenegraph.Node;
 import com.ardor3d.util.export.binary.BinaryExporter;
+import com.ardor3d.util.geom.GeometryTool;
 import com.ardor3d.util.resource.ResourceLocatorTool;
+import com.ardor3d.util.resource.ResourceSource;
 import com.ardor3d.util.resource.SimpleResourceLocator;
 import com.ardor3d.util.resource.URLResourceSource;
 
-public final class Md2ToArdorConverter{
+public final class DaeToArdorConverter{
 
     
     public static final void main(String[] args) throws IOException, URISyntaxException{
         JoglImageLoader.registerLoader();
         BinaryExporter binaryExporter=new BinaryExporter();
-        try{SimpleResourceLocator srl=new SimpleResourceLocator(Md2ToArdorConverter.class.getResource("/images"));
+        try{SimpleResourceLocator srl=new SimpleResourceLocator(DaeToArdorConverter.class.getResource("/images"));
             ResourceLocatorTool.addResourceLocator(ResourceLocatorTool.TYPE_TEXTURE,srl);
-            srl=new SimpleResourceLocator(Md2ToArdorConverter.class.getResource("/md2"));
+            srl=new SimpleResourceLocator(DaeToArdorConverter.class.getResource("/dae"));
             ResourceLocatorTool.addResourceLocator(ResourceLocatorTool.TYPE_MODEL,srl);
            } 
         catch(final URISyntaxException urise)
         {urise.printStackTrace();}
-        Md2Importer md2Importer=new Md2Importer();
-        md2Importer.setFlipTextureVertically(true);
-        URLResourceSource imageSource;
-        URLResourceSource source;
-        Spatial spatial;
-        TextureState ts;
-        File sourceFile,imageSourceFile,destFile;
+        ColladaImporter colladaImporter=new ColladaImporter();
+        final GeometryTool geomTool=new GeometryHelper();
         for(String arg:args)
             {System.out.println("Loading "+arg+" ...");
-             spatial=md2Importer.load(arg).getScene();
-             source=(URLResourceSource)ResourceLocatorTool.locateResource(ResourceLocatorTool.TYPE_MODEL,arg);
-             sourceFile=new File(source.getURL().toURI());
-             destFile=new File(sourceFile.getAbsolutePath().substring(0,sourceFile.getAbsolutePath().lastIndexOf(".md2"))+".abin");
+             final ResourceSource argSrc=ResourceLocatorTool.locateResource(ResourceLocatorTool.TYPE_MODEL,arg);
+             final Node colladaNode=colladaImporter.load(argSrc,geomTool).getScene();
+             URLResourceSource source=(URLResourceSource)ResourceLocatorTool.locateResource(ResourceLocatorTool.TYPE_MODEL,arg);
+             File sourceFile=new File(source.getURL().toURI());
+             File destFile=new File(sourceFile.getAbsolutePath().substring(0,sourceFile.getAbsolutePath().lastIndexOf(".dae"))+".abin");
              if(!destFile.exists())
                  if(!destFile.createNewFile())
                      {System.out.println(destFile.getAbsolutePath()+" cannot be created!");
                       continue;
                      }
-             imageSourceFile=new File(sourceFile.getName().substring(0,sourceFile.getName().lastIndexOf(".md2"))+".png");
-             imageSource=(URLResourceSource)ResourceLocatorTool.locateResource(ResourceLocatorTool.TYPE_TEXTURE,imageSourceFile.getName());
-             if(imageSource!=null)
-                 {ts=new TextureState();
-                  ts.setEnabled(true);
-                  ts.setTexture(TextureManager.load(new URLResourceSource(imageSource.getURL()),Texture.MinificationFilter.Trilinear,false));
-                  spatial.setRenderState(ts);
-                 }
              System.out.println("Converting "+arg+" ...");
-             binaryExporter.save(spatial,destFile);
+             binaryExporter.save(colladaNode,destFile);
              System.out.println(arg+" successfully converted");
             }
     }
