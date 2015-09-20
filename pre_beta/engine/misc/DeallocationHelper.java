7f140d5256594092cce065c3fdfeaa74bef976c6
gouessej <gouessej@48496033-b546-4ea5-bad7-dea05dfac3c5>
2015-09-20 13:55:09 +0000

    Adds a return value into the deallocator to indicate whether the deallocation has been successful

--- pre_beta/engine/misc/DeallocationHelper.java
+++ pre_beta/engine/misc/DeallocationHelper.java
@@ -71,8 +71,10 @@ public class DeallocationHelper{
          * 
          * @param directByteBuffer
          *            deallocatable byte buffer
+         *            
+         * @return <code>true</code> if the deallocation is successful, otherwise <code>false</code>
          */
-        public abstract void run(final ByteBuffer directByteBuffer);
+        public abstract boolean run(final ByteBuffer directByteBuffer);
     }
 
     public static class OracleSunOpenJdkDeallocator extends Deallocator{
@@ -95,7 +97,8 @@ public class DeallocationHelper{
         }
 
         @Override
-        public void run(final ByteBuffer directByteBuffer){
+        public boolean run(final ByteBuffer directByteBuffer){
+            boolean success=false;
             if(directByteBufferCleanerMethod!=null&&cleanerCleanMethod!=null)
                 {final boolean directByteBufferCleanerMethodWasAccessible=directByteBufferCleanerMethod.isAccessible();
                  final boolean cleanerCleanMethodMethodWasAccessible=cleanerCleanMethod.isAccessible();
@@ -105,6 +108,7 @@ public class DeallocationHelper{
                       if(cleaner!=null)
                           {cleanerCleanMethod.setAccessible(true);
                            cleanerCleanMethod.invoke(cleaner);
+                           success=true;
                           }
                      }
                  catch(IllegalAccessException|IllegalArgumentException|InvocationTargetException e)
@@ -114,6 +118,7 @@ public class DeallocationHelper{
                       cleanerCleanMethod.setAccessible(cleanerCleanMethodMethodWasAccessible);
                      }
                 }
+            return(success);
         }
     }
 
@@ -132,18 +137,21 @@ public class DeallocationHelper{
         }
 
         @Override
-        public void run(final ByteBuffer directByteBuffer){
+        public boolean run(final ByteBuffer directByteBuffer){
+            boolean success=false;
             if(directByteBufferFreeMethod!=null)
                 {final boolean directByteBufferFreeMethodWasAccessible=directByteBufferFreeMethod.isAccessible();
                  try
                      {directByteBufferFreeMethod.setAccessible(true);
                       directByteBufferFreeMethod.invoke(directByteBuffer);
+                      success=true;
                      }
                  catch(IllegalAccessException|IllegalArgumentException|InvocationTargetException e)
                      {logger.log(Level.WARNING,"The deallocation of a direct NIO buffer has failed",e);}
                  finally
                      {directByteBufferFreeMethod.setAccessible(directByteBufferFreeMethodWasAccessible);}
                 }
+            return(success);
         }
     }
 
@@ -166,7 +174,8 @@ public class DeallocationHelper{
         }
 
         @Override
-        public void run(final ByteBuffer directByteBuffer){
+        public boolean run(final ByteBuffer directByteBuffer){
+            boolean success=false;
             if(vmDirectByteBufferFreeMethod!=null&&bufferAddressField!=null)
                 {final boolean bufferAddressFieldWasAccessible=bufferAddressField.isAccessible();
                  final boolean vmDirectByteBufferFreeMethodWasAccessible=vmDirectByteBufferFreeMethod.isAccessible();
@@ -176,6 +185,7 @@ public class DeallocationHelper{
                       if(address!=null)
                           {vmDirectByteBufferFreeMethod.setAccessible(true);
                            vmDirectByteBufferFreeMethod.invoke(null,address);
+                           success=true;
                           }
                      }
                  catch(IllegalAccessException|IllegalArgumentException|InvocationTargetException e)
@@ -185,6 +195,7 @@ public class DeallocationHelper{
                       vmDirectByteBufferFreeMethod.setAccessible(vmDirectByteBufferFreeMethodWasAccessible);
                      }
                 }
+            return(success);
         }
     }
 
@@ -203,18 +214,21 @@ public class DeallocationHelper{
         }
 
         @Override
-        public void run(final ByteBuffer directByteBuffer){
+        public boolean run(final ByteBuffer directByteBuffer){
+            boolean success=false;
             if(directByteBufferFreeMethod!=null)
                 {final boolean directByteBufferFreeMethodWasAccessible=directByteBufferFreeMethod.isAccessible();
                  try
                      {directByteBufferFreeMethod.setAccessible(true);
                       directByteBufferFreeMethod.invoke(directByteBuffer);
+                      success=true;
                      }
                  catch(IllegalAccessException|IllegalArgumentException|InvocationTargetException e)
                      {logger.log(Level.WARNING,"The deallocation of a direct NIO buffer has failed",e);}
                  finally
                      {directByteBufferFreeMethod.setAccessible(directByteBufferFreeMethodWasAccessible);}
                 }
+            return(success);
         }
     }
 
@@ -735,74 +749,74 @@ public class DeallocationHelper{
     }
 
 //    public static void main(String[] args){
-//    final ByteBuffer bigEndianReadOnlyDirectByteBuffer=ByteBuffer.allocateDirect(1).order(ByteOrder.BIG_ENDIAN).asReadOnlyBuffer();
-//    final ByteBuffer bigEndianReadWriteDirectByteBuffer=ByteBuffer.allocateDirect(1).order(ByteOrder.BIG_ENDIAN);
-//    final ByteBuffer slicedBigEndianReadOnlyDirectByteBuffer=((ByteBuffer)ByteBuffer.allocateDirect(2).order(ByteOrder.BIG_ENDIAN).put((byte)0).put((byte)0).position(1).limit(2)).slice().asReadOnlyBuffer();
-//    final ByteBuffer slicedBigEndianReadWriteDirectByteBuffer=((ByteBuffer)ByteBuffer.allocateDirect(2).order(ByteOrder.BIG_ENDIAN).put((byte)0).put((byte)0).position(1).limit(2)).slice();
-//    final CharBuffer bigEndianReadOnlyDirectCharBuffer=ByteBuffer.allocateDirect(1).order(ByteOrder.BIG_ENDIAN).asReadOnlyBuffer().asCharBuffer();
-//    final CharBuffer bigEndianReadWriteDirectCharBuffer=ByteBuffer.allocateDirect(1).order(ByteOrder.BIG_ENDIAN).asCharBuffer();
-//    final DoubleBuffer bigEndianReadOnlyDirectDoubleBuffer=ByteBuffer.allocateDirect(1).order(ByteOrder.BIG_ENDIAN).asReadOnlyBuffer().asDoubleBuffer();
-//    final DoubleBuffer bigEndianReadWriteDirectDoubleBuffer=ByteBuffer.allocateDirect(1).order(ByteOrder.BIG_ENDIAN).asDoubleBuffer();
-//    final FloatBuffer bigEndianReadOnlyDirectFloatBuffer=ByteBuffer.allocateDirect(1).order(ByteOrder.BIG_ENDIAN).asReadOnlyBuffer().asFloatBuffer();
-//    final FloatBuffer bigEndianReadWriteDirectFloatBuffer=ByteBuffer.allocateDirect(1).order(ByteOrder.BIG_ENDIAN).asFloatBuffer();
-//    final IntBuffer bigEndianReadOnlyDirectIntBuffer=ByteBuffer.allocateDirect(1).order(ByteOrder.BIG_ENDIAN).asReadOnlyBuffer().asIntBuffer();
-//    final IntBuffer bigEndianReadWriteDirectIntBuffer=ByteBuffer.allocateDirect(1).order(ByteOrder.BIG_ENDIAN).asIntBuffer();
-//    final LongBuffer bigEndianReadOnlyDirectLongBuffer=ByteBuffer.allocateDirect(1).order(ByteOrder.BIG_ENDIAN).asReadOnlyBuffer().asLongBuffer();
-//    final LongBuffer bigEndianReadWriteDirectLongBuffer=ByteBuffer.allocateDirect(1).order(ByteOrder.BIG_ENDIAN).asLongBuffer();
-//    final ShortBuffer bigEndianReadOnlyDirectShortBuffer=ByteBuffer.allocateDirect(1).order(ByteOrder.BIG_ENDIAN).asReadOnlyBuffer().asShortBuffer();
-//    final ShortBuffer bigEndianReadWriteDirectShortBuffer=ByteBuffer.allocateDirect(1).order(ByteOrder.BIG_ENDIAN).asShortBuffer();
-//    final ByteBuffer littleEndianReadOnlyDirectByteBuffer=ByteBuffer.allocateDirect(1).order(ByteOrder.LITTLE_ENDIAN).asReadOnlyBuffer();
-//    final ByteBuffer littleEndianReadWriteDirectByteBuffer=ByteBuffer.allocateDirect(1).order(ByteOrder.LITTLE_ENDIAN);
-//    final ByteBuffer slicedLittleEndianReadOnlyDirectByteBuffer=((ByteBuffer)ByteBuffer.allocateDirect(2).order(ByteOrder.LITTLE_ENDIAN).put((byte)0).put((byte)0).position(1).limit(2)).slice().asReadOnlyBuffer();
-//    final ByteBuffer slicedLittleEndianReadWriteDirectByteBuffer=((ByteBuffer)ByteBuffer.allocateDirect(2).order(ByteOrder.LITTLE_ENDIAN).put((byte)0).put((byte)0).position(1).limit(2)).slice();
-//    final CharBuffer littleEndianReadOnlyDirectCharBuffer=ByteBuffer.allocateDirect(1).order(ByteOrder.LITTLE_ENDIAN).asReadOnlyBuffer().asCharBuffer();
-//    final CharBuffer littleEndianReadWriteDirectCharBuffer=ByteBuffer.allocateDirect(1).order(ByteOrder.LITTLE_ENDIAN).asCharBuffer();
-//    final DoubleBuffer littleEndianReadOnlyDirectDoubleBuffer=ByteBuffer.allocateDirect(1).order(ByteOrder.LITTLE_ENDIAN).asReadOnlyBuffer().asDoubleBuffer();
-//    final DoubleBuffer littleEndianReadWriteDirectDoubleBuffer=ByteBuffer.allocateDirect(1).order(ByteOrder.LITTLE_ENDIAN).asDoubleBuffer();
-//    final FloatBuffer littleEndianReadOnlyDirectFloatBuffer=ByteBuffer.allocateDirect(1).order(ByteOrder.LITTLE_ENDIAN).asReadOnlyBuffer().asFloatBuffer();
-//    final FloatBuffer littleEndianReadWriteDirectFloatBuffer=ByteBuffer.allocateDirect(1).order(ByteOrder.LITTLE_ENDIAN).asFloatBuffer();
-//    final IntBuffer littleEndianReadOnlyDirectIntBuffer=ByteBuffer.allocateDirect(1).order(ByteOrder.LITTLE_ENDIAN).asReadOnlyBuffer().asIntBuffer();
-//    final IntBuffer littleEndianReadWriteDirectIntBuffer=ByteBuffer.allocateDirect(1).order(ByteOrder.LITTLE_ENDIAN).asIntBuffer();
-//    final LongBuffer littleEndianReadOnlyDirectLongBuffer=ByteBuffer.allocateDirect(1).order(ByteOrder.LITTLE_ENDIAN).asReadOnlyBuffer().asLongBuffer();
-//    final LongBuffer littleEndianReadWriteDirectLongBuffer=ByteBuffer.allocateDirect(1).order(ByteOrder.LITTLE_ENDIAN).asLongBuffer();
-//    final ShortBuffer littleEndianReadOnlyDirectShortBuffer=ByteBuffer.allocateDirect(1).order(ByteOrder.LITTLE_ENDIAN).asReadOnlyBuffer().asShortBuffer();
-//    final ShortBuffer littleEndianReadWriteDirectShortBuffer=ByteBuffer.allocateDirect(1).order(ByteOrder.LITTLE_ENDIAN).asShortBuffer();
-//    final List<Buffer> buffers=new ArrayList<>();
-//    buffers.add(bigEndianReadOnlyDirectByteBuffer);
-//    buffers.add(bigEndianReadWriteDirectByteBuffer);
-//    buffers.add(slicedBigEndianReadOnlyDirectByteBuffer);
-//    buffers.add(slicedBigEndianReadWriteDirectByteBuffer);
-//    buffers.add(bigEndianReadOnlyDirectCharBuffer);
-//    buffers.add(bigEndianReadWriteDirectCharBuffer);
-//    buffers.add(bigEndianReadOnlyDirectDoubleBuffer);
-//    buffers.add(bigEndianReadWriteDirectDoubleBuffer);
-//    buffers.add(bigEndianReadOnlyDirectFloatBuffer);
-//    buffers.add(bigEndianReadWriteDirectFloatBuffer);
-//    buffers.add(bigEndianReadOnlyDirectIntBuffer);
-//    buffers.add(bigEndianReadWriteDirectIntBuffer);
-//    buffers.add(bigEndianReadOnlyDirectLongBuffer);
-//    buffers.add(bigEndianReadWriteDirectLongBuffer);
-//    buffers.add(bigEndianReadOnlyDirectShortBuffer);
-//    buffers.add(bigEndianReadWriteDirectShortBuffer);
-//    buffers.add(littleEndianReadOnlyDirectByteBuffer);
-//    buffers.add(littleEndianReadWriteDirectByteBuffer);
-//    buffers.add(slicedLittleEndianReadOnlyDirectByteBuffer);
-//    buffers.add(slicedLittleEndianReadWriteDirectByteBuffer);
-//    buffers.add(littleEndianReadOnlyDirectCharBuffer);
-//    buffers.add(littleEndianReadWriteDirectCharBuffer);
-//    buffers.add(littleEndianReadOnlyDirectDoubleBuffer);
-//    buffers.add(littleEndianReadWriteDirectDoubleBuffer);
-//    buffers.add(littleEndianReadOnlyDirectFloatBuffer);
-//    buffers.add(littleEndianReadWriteDirectFloatBuffer);
-//    buffers.add(littleEndianReadOnlyDirectIntBuffer);
-//    buffers.add(littleEndianReadWriteDirectIntBuffer);
-//    buffers.add(littleEndianReadOnlyDirectLongBuffer);
-//    buffers.add(littleEndianReadWriteDirectLongBuffer);
-//    buffers.add(littleEndianReadOnlyDirectShortBuffer);
-//    buffers.add(littleEndianReadWriteDirectShortBuffer);
-//    final DeallocationHelper helper=new DeallocationHelper();
-//    for(final Buffer buffer:buffers)
-//        helper.deallocate(buffer);
-//}
+//        final ByteBuffer bigEndianReadOnlyDirectByteBuffer=ByteBuffer.allocateDirect(1).order(ByteOrder.BIG_ENDIAN).asReadOnlyBuffer();
+//        final ByteBuffer bigEndianReadWriteDirectByteBuffer=ByteBuffer.allocateDirect(1).order(ByteOrder.BIG_ENDIAN);
+//        final ByteBuffer slicedBigEndianReadOnlyDirectByteBuffer=((ByteBuffer)ByteBuffer.allocateDirect(2).order(ByteOrder.BIG_ENDIAN).put((byte)0).put((byte)0).position(1).limit(2)).slice().asReadOnlyBuffer();
+//        final ByteBuffer slicedBigEndianReadWriteDirectByteBuffer=((ByteBuffer)ByteBuffer.allocateDirect(2).order(ByteOrder.BIG_ENDIAN).put((byte)0).put((byte)0).position(1).limit(2)).slice();
+//        final CharBuffer bigEndianReadOnlyDirectCharBuffer=ByteBuffer.allocateDirect(1).order(ByteOrder.BIG_ENDIAN).asReadOnlyBuffer().asCharBuffer();
+//        final CharBuffer bigEndianReadWriteDirectCharBuffer=ByteBuffer.allocateDirect(1).order(ByteOrder.BIG_ENDIAN).asCharBuffer();
+//        final DoubleBuffer bigEndianReadOnlyDirectDoubleBuffer=ByteBuffer.allocateDirect(1).order(ByteOrder.BIG_ENDIAN).asReadOnlyBuffer().asDoubleBuffer();
+//        final DoubleBuffer bigEndianReadWriteDirectDoubleBuffer=ByteBuffer.allocateDirect(1).order(ByteOrder.BIG_ENDIAN).asDoubleBuffer();
+//        final FloatBuffer bigEndianReadOnlyDirectFloatBuffer=ByteBuffer.allocateDirect(1).order(ByteOrder.BIG_ENDIAN).asReadOnlyBuffer().asFloatBuffer();
+//        final FloatBuffer bigEndianReadWriteDirectFloatBuffer=ByteBuffer.allocateDirect(1).order(ByteOrder.BIG_ENDIAN).asFloatBuffer();
+//        final IntBuffer bigEndianReadOnlyDirectIntBuffer=ByteBuffer.allocateDirect(1).order(ByteOrder.BIG_ENDIAN).asReadOnlyBuffer().asIntBuffer();
+//        final IntBuffer bigEndianReadWriteDirectIntBuffer=ByteBuffer.allocateDirect(1).order(ByteOrder.BIG_ENDIAN).asIntBuffer();
+//        final LongBuffer bigEndianReadOnlyDirectLongBuffer=ByteBuffer.allocateDirect(1).order(ByteOrder.BIG_ENDIAN).asReadOnlyBuffer().asLongBuffer();
+//        final LongBuffer bigEndianReadWriteDirectLongBuffer=ByteBuffer.allocateDirect(1).order(ByteOrder.BIG_ENDIAN).asLongBuffer();
+//        final ShortBuffer bigEndianReadOnlyDirectShortBuffer=ByteBuffer.allocateDirect(1).order(ByteOrder.BIG_ENDIAN).asReadOnlyBuffer().asShortBuffer();
+//        final ShortBuffer bigEndianReadWriteDirectShortBuffer=ByteBuffer.allocateDirect(1).order(ByteOrder.BIG_ENDIAN).asShortBuffer();
+//        final ByteBuffer littleEndianReadOnlyDirectByteBuffer=ByteBuffer.allocateDirect(1).order(ByteOrder.LITTLE_ENDIAN).asReadOnlyBuffer();
+//        final ByteBuffer littleEndianReadWriteDirectByteBuffer=ByteBuffer.allocateDirect(1).order(ByteOrder.LITTLE_ENDIAN);
+//        final ByteBuffer slicedLittleEndianReadOnlyDirectByteBuffer=((ByteBuffer)ByteBuffer.allocateDirect(2).order(ByteOrder.LITTLE_ENDIAN).put((byte)0).put((byte)0).position(1).limit(2)).slice().asReadOnlyBuffer();
+//        final ByteBuffer slicedLittleEndianReadWriteDirectByteBuffer=((ByteBuffer)ByteBuffer.allocateDirect(2).order(ByteOrder.LITTLE_ENDIAN).put((byte)0).put((byte)0).position(1).limit(2)).slice();
+//        final CharBuffer littleEndianReadOnlyDirectCharBuffer=ByteBuffer.allocateDirect(1).order(ByteOrder.LITTLE_ENDIAN).asReadOnlyBuffer().asCharBuffer();
+//        final CharBuffer littleEndianReadWriteDirectCharBuffer=ByteBuffer.allocateDirect(1).order(ByteOrder.LITTLE_ENDIAN).asCharBuffer();
+//        final DoubleBuffer littleEndianReadOnlyDirectDoubleBuffer=ByteBuffer.allocateDirect(1).order(ByteOrder.LITTLE_ENDIAN).asReadOnlyBuffer().asDoubleBuffer();
+//        final DoubleBuffer littleEndianReadWriteDirectDoubleBuffer=ByteBuffer.allocateDirect(1).order(ByteOrder.LITTLE_ENDIAN).asDoubleBuffer();
+//        final FloatBuffer littleEndianReadOnlyDirectFloatBuffer=ByteBuffer.allocateDirect(1).order(ByteOrder.LITTLE_ENDIAN).asReadOnlyBuffer().asFloatBuffer();
+//        final FloatBuffer littleEndianReadWriteDirectFloatBuffer=ByteBuffer.allocateDirect(1).order(ByteOrder.LITTLE_ENDIAN).asFloatBuffer();
+//        final IntBuffer littleEndianReadOnlyDirectIntBuffer=ByteBuffer.allocateDirect(1).order(ByteOrder.LITTLE_ENDIAN).asReadOnlyBuffer().asIntBuffer();
+//        final IntBuffer littleEndianReadWriteDirectIntBuffer=ByteBuffer.allocateDirect(1).order(ByteOrder.LITTLE_ENDIAN).asIntBuffer();
+//        final LongBuffer littleEndianReadOnlyDirectLongBuffer=ByteBuffer.allocateDirect(1).order(ByteOrder.LITTLE_ENDIAN).asReadOnlyBuffer().asLongBuffer();
+//        final LongBuffer littleEndianReadWriteDirectLongBuffer=ByteBuffer.allocateDirect(1).order(ByteOrder.LITTLE_ENDIAN).asLongBuffer();
+//        final ShortBuffer littleEndianReadOnlyDirectShortBuffer=ByteBuffer.allocateDirect(1).order(ByteOrder.LITTLE_ENDIAN).asReadOnlyBuffer().asShortBuffer();
+//        final ShortBuffer littleEndianReadWriteDirectShortBuffer=ByteBuffer.allocateDirect(1).order(ByteOrder.LITTLE_ENDIAN).asShortBuffer();
+//        final List<Buffer> buffers=new ArrayList<>();
+//        buffers.add(bigEndianReadOnlyDirectByteBuffer);
+//        buffers.add(bigEndianReadWriteDirectByteBuffer);
+//        buffers.add(slicedBigEndianReadOnlyDirectByteBuffer);
+//        buffers.add(slicedBigEndianReadWriteDirectByteBuffer);
+//        buffers.add(bigEndianReadOnlyDirectCharBuffer);
+//        buffers.add(bigEndianReadWriteDirectCharBuffer);
+//        buffers.add(bigEndianReadOnlyDirectDoubleBuffer);
+//        buffers.add(bigEndianReadWriteDirectDoubleBuffer);
+//        buffers.add(bigEndianReadOnlyDirectFloatBuffer);
+//        buffers.add(bigEndianReadWriteDirectFloatBuffer);
+//        buffers.add(bigEndianReadOnlyDirectIntBuffer);
+//        buffers.add(bigEndianReadWriteDirectIntBuffer);
+//        buffers.add(bigEndianReadOnlyDirectLongBuffer);
+//        buffers.add(bigEndianReadWriteDirectLongBuffer);
+//        buffers.add(bigEndianReadOnlyDirectShortBuffer);
+//        buffers.add(bigEndianReadWriteDirectShortBuffer);
+//        buffers.add(littleEndianReadOnlyDirectByteBuffer);
+//        buffers.add(littleEndianReadWriteDirectByteBuffer);
+//        buffers.add(slicedLittleEndianReadOnlyDirectByteBuffer);
+//        buffers.add(slicedLittleEndianReadWriteDirectByteBuffer);
+//        buffers.add(littleEndianReadOnlyDirectCharBuffer);
+//        buffers.add(littleEndianReadWriteDirectCharBuffer);
+//        buffers.add(littleEndianReadOnlyDirectDoubleBuffer);
+//        buffers.add(littleEndianReadWriteDirectDoubleBuffer);
+//        buffers.add(littleEndianReadOnlyDirectFloatBuffer);
+//        buffers.add(littleEndianReadWriteDirectFloatBuffer);
+//        buffers.add(littleEndianReadOnlyDirectIntBuffer);
+//        buffers.add(littleEndianReadWriteDirectIntBuffer);
+//        buffers.add(littleEndianReadOnlyDirectLongBuffer);
+//        buffers.add(littleEndianReadWriteDirectLongBuffer);
+//        buffers.add(littleEndianReadOnlyDirectShortBuffer);
+//        buffers.add(littleEndianReadWriteDirectShortBuffer);
+//        final DeallocationHelper helper=new DeallocationHelper();
+//        for(final Buffer buffer:buffers)
+//            helper.deallocate(buffer);
+//    }
 
 }
