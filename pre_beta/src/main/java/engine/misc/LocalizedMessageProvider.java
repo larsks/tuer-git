f994c2d474e07367399cee70fc0f275db554a917
gouessej <gouessej@48496033-b546-4ea5-bad7-dea05dfac3c5>
2015-10-15 19:30:33 +0000

    Reads the language code from the configuration file if any

--- pre_beta/src/main/java/engine/misc/LocalizedMessageProvider.java
+++ pre_beta/src/main/java/engine/misc/LocalizedMessageProvider.java
@@ -17,17 +17,53 @@
  */
 package engine.misc;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Locale;
+import java.util.Properties;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
+/**
+ * Provider of localized messages. It looks for the language chosen by the end user in the configuration file. It uses the 
+ * default language of the operating system as a fallback.
+ * 
+ * @author Julien Gouesse
+ *
+ */
 public class LocalizedMessageProvider{
+	
+	private static final Logger logger=Logger.getLogger(LocalizedMessageProvider.class.getCanonicalName());
 	
 	private ResourceBundle resourceBundle;
 
-	public LocalizedMessageProvider(){
+	public LocalizedMessageProvider(final String gameFilesSubDirPath){
 		super();
 		Locale locale=Locale.getDefault();
-		//TODO look at the file that contains the settings as the end user might have chosen another language
+		//TODO move the reading of the configuration file into a separate class
+		//looks at the file that contains the settings as the end user might have chosen another language
+		final File configFile=new File(System.getProperty("user.home")+"/"+gameFilesSubDirPath,"config");
+		if(configFile.exists())
+		    {//tries to read the ISO 639 alpha-2 or alpha-3 language code
+			 final Properties properties=new Properties();
+			 try(final FileReader fileReader=new FileReader(configFile);final BufferedReader bufferedReader=new BufferedReader(fileReader))
+			     {properties.load(bufferedReader);
+				  final String languageCode=properties.getProperty("LANGUAGE","en");
+				  locale=new Locale(languageCode);
+				  logger.log(Level.INFO,"Language code \""+languageCode+"\" found in the configuration file "+configFile.getAbsolutePath());
+			     }
+			 catch(IOException ioe)
+			     {//something wrong has just happened while reading the configuration file
+		          logger.log(Level.WARNING,"Cannot read the configuration file "+configFile.getAbsolutePath(),ioe);
+			     }
+		    }
+		else
+		    {//the configuration file is absent
+			 logger.log(Level.WARNING,"Cannot find the configuration file "+configFile.getAbsolutePath());
+		    }
 		resourceBundle=ResourceBundle.getBundle("i18n.MessagesBundle",locale);
 	}
 	
