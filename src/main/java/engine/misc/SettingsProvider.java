bd7db9edb645ab08e2e3535cefa92568f846eb2c
gouessej <gouessej@48496033-b546-4ea5-bad7-dea05dfac3c5>
2015-10-15 22:22:02 +0000

    Removes a useless import from the provider of settings

--- src/main/java/engine/misc/SettingsProvider.java
+++ src/main/java/engine/misc/SettingsProvider.java
@@ -23,7 +23,6 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Date;
 import java.util.Locale;
 import java.util.Properties;
 import java.util.logging.Level;
