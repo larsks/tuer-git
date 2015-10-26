357ee7446c35299e200c27e21c6414b3f22b4f16
gouessej <gouessej@48496033-b546-4ea5-bad7-dea05dfac3c5>
2015-10-26 22:55:56 +0000

    Oops, I forgot the license header in the previous file

--- pre_beta/src/test/java/jfpsm/TestComputeCollisionBoundingVolumes.java
+++ pre_beta/src/test/java/jfpsm/TestComputeCollisionBoundingVolumes.java
@@ -1,3 +1,20 @@
+/**
+ * Copyright (c) 2006-2015 Julien Gouesse
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
 package jfpsm;
 
 import java.net.URL;
@@ -19,7 +36,7 @@ public class TestComputeCollisionBoundingVolumes {
 
 	public static void main(String[] args) {
 		JoglImageLoader.registerLoader();
-		final URL mapUrl=Level.class.getResource("/images/containermap1.png");
+		final URL mapUrl=Level.class.getResource("/images/containermap.png");
     	final URLResourceSource mapSource=new URLResourceSource(mapUrl);
     	final Image map=ImageLoaderUtil.loadImage(mapSource,false);
     	final Boolean[][] collisionMap=new Boolean[map.getWidth()][map.getHeight()];
