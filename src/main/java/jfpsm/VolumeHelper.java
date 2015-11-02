4a2223f0397795c56c8d2c4053b3e4a4afa73090
gouessej <gouessej@48496033-b546-4ea5-bad7-dea05dfac3c5>
2015-11-02 23:39:49 +0000

    Fixes several bugs in the computation of the bounding boxes and computes the bounding boxes of the floor and of the ceiling

--- src/main/java/jfpsm/VolumeHelper.java
+++ src/main/java/jfpsm/VolumeHelper.java
@@ -20,8 +20,10 @@ package jfpsm;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map.Entry;
+
 import com.ardor3d.bounding.BoundingBox;
 import com.ardor3d.math.Vector3;
+
 import jfpsm.ArrayHelper.Vector2i;
 
 /**
@@ -42,16 +44,20 @@ public class VolumeHelper{
 	 * @param fullArrayMap map of full arrays
 	 * @param z0 first applicate of the bounding boxes for the main layer
 	 * @param z1 second applicate of the bounding boxes for the main layer
-	 * @param generateFloorBoundingBox <code>true</code> if a single bounding box below the main layer has to be computed except if the map is empty, otherwise <code>false</code> //TODO
-	 * @param generateCeilingBoundingBox <code>true</code> if a single bounding box above the main layer has to be computed except if the map is empty, otherwise <code>false</code> //TODO
+	 * @param generateFloorBoundingBox <code>true</code> if a single bounding box below the main layer has to be computed, otherwise <code>false</code>
+	 * @param generateCeilingBoundingBox <code>true</code> if a single bounding box above the main layer has to be computed, otherwise <code>false</code>
 	 * @return list of 3D bounding boxes
 	 */
 	public <T> List<BoundingBox> computeBoundingBoxListFromFullArrayMap(final java.util.Map<Vector2i,T[][]> fullArrayMap,
 			final double z0,final double z1,final boolean generateFloorBoundingBox,final boolean generateCeilingBoundingBox){
 		final List<BoundingBox> boundingBoxList=new ArrayList<>();
     	final Vector2i zero=new Vector2i(0,0);
+    	int smallestRowIndex=Integer.MAX_VALUE;
+		int biggestRowIndex=Integer.MIN_VALUE;
+		int smallestColumnIndex=Integer.MAX_VALUE;
+		int biggestColumnIndex=Integer.MIN_VALUE;
     	//extent of the applicates
-    	final double zExtent=Math.abs(z1-z0);
+    	final double zExtent=Math.abs(z1-z0)/2.0;
     	//for each full array
     	for(final Entry<Vector2i,T[][]> fullArrayMapEntry:fullArrayMap.entrySet())
     	    {final Vector2i location=fullArrayMapEntry.getKey()==null?zero:fullArrayMapEntry.getKey();
@@ -68,17 +74,46 @@ public class VolumeHelper{
     			       //ordinate
     			       final int y0=location.getY();
     			       final int y1=y0+fullArrayRowCount;
+    			       //updates the minimum indices and the maximum indices
+    			       smallestColumnIndex=Math.min(smallestColumnIndex,x0);
+    			       smallestRowIndex=Math.min(smallestRowIndex,y0);
+    			       biggestColumnIndex=Math.max(biggestColumnIndex,x1);
+    			       biggestRowIndex=Math.max(biggestRowIndex,y1);
     			       //other extents
-    			       final double xExtent=Math.abs(x1-x0);
-    			       final double yExtent=Math.abs(y1-y0);
+    			       final double xExtent=Math.abs(x1-x0)/2.0;
+    			       final double yExtent=Math.abs(y1-y0)/2.0;
     			       //center
-    			       final Vector3 center=new Vector3(xExtent/2.0,yExtent/2.0,zExtent/2.0);
+    			       final Vector3 center=new Vector3((x0+x1)/2.0,(y0+y1)/2.0,(z0+z1)/2.0);
     			       //bounding box
     			       final BoundingBox boundingBox=new BoundingBox(center,xExtent,yExtent,zExtent);
     			       //adds the bounding box into the list
     			       boundingBoxList.add(boundingBox);
     		          }
     		     }
+    	    }
+    	if(!boundingBoxList.isEmpty()&&(generateFloorBoundingBox||generateCeilingBoundingBox))
+    	    {final double x2=smallestColumnIndex;
+    		 final double x3=biggestColumnIndex;
+    		 final double y2=smallestRowIndex;
+    		 final double y3=biggestRowIndex;
+    	     if(generateFloorBoundingBox)
+    	         {final double z2=Math.min(z0,z1);
+    	          final double z3=z2-(zExtent*2.0);
+    	          final double xExtent=Math.abs(x2-x3)/2.0;
+			      final double yExtent=Math.abs(y2-y3)/2.0;
+    	          final Vector3 center=new Vector3((x2+x3)/2.0,(y2+y3)/2.0,(z2+z3)/2.0);
+    	          final BoundingBox boundingBox=new BoundingBox(center,xExtent,yExtent,zExtent);
+    	          boundingBoxList.add(boundingBox);
+    	         }
+    	     if(generateCeilingBoundingBox)
+    	         {final double z2=Math.max(z0,z1);
+   	              final double z3=z2+(zExtent*2.0);
+   	              final double xExtent=Math.abs(x2-x3)/2.0;
+			      final double yExtent=Math.abs(y2-y3)/2.0;
+   	              final Vector3 center=new Vector3((x2+x3)/2.0,(y2+y3)/2.0,(z2+z3)/2.0);
+   	              final BoundingBox boundingBox=new BoundingBox(center,xExtent,yExtent,zExtent);
+   	              boundingBoxList.add(boundingBox);
+    	         }
     	    }
     	return(boundingBoxList);
 	}
