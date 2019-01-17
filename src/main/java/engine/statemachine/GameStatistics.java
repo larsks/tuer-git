a5de35cac60e01bec4397adf23003c5b833275e9
gouessej <gouessej@48496033-b546-4ea5-bad7-dea05dfac3c5>
2019-01-17 21:20:32 +0000

    Updates the copyright notices

--- src/main/java/engine/statemachine/GameStatistics.java
+++ src/main/java/engine/statemachine/GameStatistics.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2006-2017 Julien Gouesse
+ * Copyright (c) 2006-2019 Julien Gouesse
  * 
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
