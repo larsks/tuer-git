65de99b058dfe04f4c6837508bad36ca40fb5bf6
gouessej <gouessej@48496033-b546-4ea5-bad7-dea05dfac3c5>
2021-01-22 21:09:29 +0000

    Updates the copyright year

--- src/main/java/engine/misc/DeallocationHelper.java
+++ src/main/java/engine/misc/DeallocationHelper.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2006-2020 Julien Gouesse This program is free software; you can
+ * Copyright (c) 2006-2021 Julien Gouesse This program is free software; you can
  * redistribute it and/or modify it under the terms of the GNU General Public
  * License as published by the Free Software Foundation; either version 2 of the
  * License, or (at your option) any later version. This program is distributed
